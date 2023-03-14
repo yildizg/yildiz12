2c6944075060713f9ac6835173990437b60dfc60
Yildiz <yldzgny34@gmail.com>
2023-03-14 14:57:46 +0100

    Initial commit

---
+++ src/ch01_JavaIntro/C01_JavaGiris.java
@@ -0,0 +1,15 @@
+package ch01_JavaIntro;
+
+public class C01_JavaGiris {
+    public static void main(String[] args) {
+
+
+                System.out.println(" Ad-Soyad: Gülhan Tezcan");
+                System.out.println(" Memleket: Sivas");
+                System.out.println(" Meslek  : Ögretmen");
+                System.out.println(" Yas     : 37");
+                System.out.println("         : " + 37);
+
+
+    }
+}
